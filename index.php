568b4ddd22dd168d76ed0f87dbc2cc2f3628d0aa
Maximpop <rudkovpop@gmail.com>
2017-01-17 10:45:33 +0000

    Add files via upload
    Index fyrir php verkefni1

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+ 
+ <head>
+
+        <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+        
+<META HTTP-EQUIV="Expires" CONTENT="-1">
+        
+<style type="text/css">
+  
+          body {
+                border:0px;
+                margin:0px;
+                overflow:hidden;
+            }
+            .wrapper {
+                position:absolute;
+                top:15%;
+                left:0px;
+                right:0px;
+                width:100%;
+                height:auto;
+            }
+            .sammy {
+                float:left;
+                width:100%;
+                height:auto;
+                text-align:center;
+            }
+            .message {
+                float:left;
+                width:100%;
+                height:auto;
+                font-size:36px;
+                text-align:center;
+                font-family:Arial,Helvetica,sans-serif;
+                margin-top:25px;
+            }
+        </style>
+
+    </head>
+    
+<body>
+        
+<p>Hello World</p>
+</body>
+</html>
